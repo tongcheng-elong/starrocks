68141c45774d501da12e01ee5224a71850df80bc
wxp <wxp4532@ly.com>
2024-12-11 20:28:09 +0800

    fix OpenCsv default seprate

--- fe/fe-core/src/main/java/com/starrocks/connector/hive/HiveMetastoreApiConverter.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/hive/HiveMetastoreApiConverter.java
@@ -20,6 +20,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.opencsv.CSVWriter;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.HiveTable;
@@ -578,7 +579,9 @@ public class HiveMetastoreApiConverter {
         if (fieldDelim.isEmpty()) {
             // Support for hive org.apache.hadoop.hive.serde2.OpenCSVSerde
             // https://cwiki.apache.org/confluence/display/hive/csv+serde
-            fieldDelim = parameters.getOrDefault(OpenCSVSerde.SEPARATORCHAR, "");
+            if (parameters.getOrDefault(HIVE_TABLE_SERDE_LIB, null).equals(HiveClassNames.OPEN_CSV_SERDE_CLASS)) {
+                fieldDelim = parameters.getOrDefault(OpenCSVSerde.SEPARATORCHAR, String.valueOf(CSVWriter.DEFAULT_SEPARATOR));
+            }
         }
         String lineDelim = parameters.getOrDefault(serdeConstants.LINE_DELIM, "");
         String mapkeyDelim = parameters.getOrDefault(serdeConstants.MAPKEY_DELIM, "");
